93d2c05a930ee783fc25c8664d123b18eb0733c9
DrettaM <mildretta.martin@outlook.com>
2021-11-17 20:54:19 -0500

    Edited table columns

--- OnlineShopCreateDB.sql
+++ OnlineShopCreateDB.sql
@@ -1,14 +1,20 @@
 /* SalesAnalytics.sql
  * Author: Mildretta Martin
  * Date Created: 10/29/2021
+ * Last Revision: 11/17/2021
  * Description: Sales analytics for Online Shop
  *
  * Tables: 
-			Customer (CustID, Name, Email, Address1, Address2, City, State, PostalCode, Country, Phone) 
-			Items (ItemSKU, Category, ItemDesc, PricePerUnit, ItemStatus)
-			Orders (OrderID, CustID, ItemSKU, ItemsQty, OrderAmt, OrderDate, ShipDate, ShipVia, ShipCost, ShipName, ShipAddr1, ShipAddr2, ShipCity, ShipState, ShipPostalCode, ShipCountry)
+			Customer (CustID, CompanyName, FirstName, LastName, Email, Addr1, Addr2, City, State_Province, PostalCode, Country, Phone) 
+			Inventory (ItemSKU, Category, ItemDesc, UOM, PricePerUnit)
+			Orders (OrderID, CustID, ItemSKU, Item1Qty, Item2SKU, Item2Qty, TotQty, OrderAmt, OrderDate)
  *
  * Stored Procedures:
+			1. ImportData - Imports data from csv files into tables
+			2. CreateSalesReport - Displays by inventory category, the number of items sold and to whom
+			3. CreateRevenueReport - Displays sales totals by month and year
+			4. PopularItems - Displays items that have more than 200 units purchased
+		
  *    
 */
 
@@ -51,45 +57,54 @@ GO
 
 CREATE TABLE Customer (
     CustID int PRIMARY KEY,
-	Name nvarchar(30),
-	Email nvarchar(50),
-	Address1 nvarchar(255),
-	Address2 nvarchar(255) Null,
-	City nvarchar(30),
-	State nvarchar(2),
+	CompanyName varchar(35) NULL,
+	FirstName varchar(35) NOT NULL,
+	LastName varchar(35) NOT NULL,
+	Email varchar(50),
+	Addr1 varchar(255),
+	Addr2 varchar(255) Null,
+	City varchar(30),
+	State_Province varchar(2),
 	PostalCode int ,
-	Country nvarchar(30),
-	Phone int
+	Country varchar(30),
+	Phone int,
+	DateCreated datetime NOT NULL DEFAULT GETDATE()
     );
 
-CREATE TABLE Items (
-    ItemSKU int PRIMARY KEY,
-    Category nvarchar(50),
-    ItemDesc nvarchar(255),
-    PricePerUnit smallmoney,
-    ItemStatus nvarchar(15)
-    );
+CREATE TABLE Inventory (
+    ItemSKU nvarchar(25) PRIMARY KEY,
+    Category varchar(50),
+    ItemDesc varchar(255),
+    OnHand int,
+	UOM varchar(25),
+	PricePerUnit smallmoney
+	);
 
 CREATE TABLE Orders (
     OrderID int PRIMARY KEY,
     CustID int FOREIGN KEY(CustID) REFERENCES Customer(CustID) ,
-    ItemSKU int FOREIGN KEY(ItemSKU) REFERENCES Items(ItemSKU),
-    ItemsQty int,
-    OrderAmt smallmoney,
-    OrderDate datetime,
-    ShipDate datetime,
-    ShipVia varchar(4) NOT NULL,
-    ShipCost smallmoney,
-    ShipName nvarchar(30),
-    ShipAddr1 nvarchar(255),
-    ShipAddr2 nvarchar(255),
-    ShipCity nvarchar(30),
-    ShipState nvarchar(2),
-    ShipPostalCode int,
-    ShipCountry nvarchar(30)
+    ItemSKU nvarchar(25) FOREIGN KEY(ItemSKU) REFERENCES Inventory(ItemSKU),
+    Item1Qty int,
+	Item2SKU nvarchar(25) FOREIGN KEY(Item2SKU) REFERENCES Inventory(ItemSKU),
+    TotQty int,
+	OrderAmt smallmoney,
+    OrderDate datetime NOT NULL DEFAULT GETDATE(),
     );
 
 /*************************************************************************
 *********************** STORED PROCEDURES ********************************
 **************************************************************************/
 
+
+/*CREATE OR ALTER PROCEDURE CreateSalesReport
+@Category varchar(255)
+AS
+SELECT SUM(so.SalesAmount) AS 'SalesAmount'
+	   ,spl.LevelName AS 'Level'
+	   ,CONCAT(sp.LastName,', ',sp.FirstName) AS 'FullName' 
+FROM Sales.SalesPerson sp
+LEFT OUTER JOIN Sales.SalesOrder so ON so.SalesPerson = sp.Id
+LEFT OUTER JOIN Sales.SalesPersonLevel spl ON spl.Id = sp.LevelId
+WHERE spl.LevelName = @LevelName
+GROUP BY spl.LevelName, sp.LastName, sp.FirstName;
+GO */
